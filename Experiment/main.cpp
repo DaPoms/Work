bb6e504500c66c9852307b0b97823708b97094f9
DaPoms <DanielTripolitan@gmail.com>
2024-07-25 15:46:39 -0400

    5.6

--- Experiment/main.cpp
+++ Experiment/main.cpp
@@ -10,24 +10,11 @@ int main()
 	
 	system("CLS");
 
-	int y{};
-	std::cin >> y;
+	int x {6};
+	int y {4};
 
-	const int x{y};
-	std::cout << x;
+	std::cout << ((x > y) ? "X Greater than y" : " X Less than Y"); //Cool format for if else statements
 	
-	
-	//std::bitset<8> x{0b1001'0010};
-	
-	
-	
-	
-	
-	//int x { 0b10010}; //Binary  literal 
-	//Ox Hexadecimal literal 
-	// " ' " can allow spaces
-
-	//std::cout << x;
 	
 	
 }
