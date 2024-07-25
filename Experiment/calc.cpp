57829e00857c3a6fd07ab40de21dd596d027cb09
DaPoms <DanielTripolitan@gmail.com>
2024-07-25 16:32:53 -0400

    5.7 inline vars

---
+++ Experiment/calc.cpp
@@ -0,0 +1,6 @@
+#include "add.h"
+
+int twoOfAdd()
+{
+    return add3_2() * add3_2();
+}
--- Experiment/main.cpp
+++ Experiment/main.cpp
@@ -3,17 +3,14 @@
 // #include <cstdint> reminder! This is used for specific int sizes
 //#include <bitset>
 #include <iostream>
+#include "add.h"
 
 int main()
 {
 
 	
-	system("CLS");
 
-	int x {6};
-	int y {4};
-
-	std::cout << ((x > y) ? "X Greater than y" : " X Less than Y"); //Cool format for if else statements
+	std::cout << add3_2();
 	
 	
 	
