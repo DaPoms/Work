eaddac425822f8dcb800f088861579a6d2f161e7
DaPoms <DanielTripolitan@gmail.com>
2025-06-04 11:30:32 -0400

    Leet code 6/4/25

---
+++ leetcode/remove_element/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//DIY approach (doesen't remove elements, just ignores them by moving all the other elements to the start of the vector and k defines the "new" size of the vector)
+int removeElement(vector<int>& nums, int val) 
+{
+    int k{0}; //K is the count of elements that are != val
+    for(int i{0}; i < nums.size(); i++)
+    {
+        if(nums[i] != val)
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    
+    }        
+    return k;
+}
+
+
+//library approach
+/* int removeElement(vector<int>& nums, int val) 
+{
+    int k{0}; //K is the count of elements that are != val
+    for(int i{0}; i < nums.size(); i++)
+    {
+        if(nums[i] == val)
+        {
+            nums.erase(nums.begin() + i, (nums.begin() + (i + 1) ) );
+            i--;
+        }
+        else    
+            k++;
+    }        
+    return k;
+} */
+
+int main()
+{
+    vector<int> test = {3,2,2,3};
+    removeElement(test, 3);
+
+    for(int num : test)
+        cout << num << " ";
+}
