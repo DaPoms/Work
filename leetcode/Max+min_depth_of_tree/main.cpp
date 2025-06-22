4f3cfac6a6ca5cafd430b1f0d9cca44dd9cbf79e
DaPoms <DanielTripolitan@gmail.com>
2025-06-22 09:16:46 -0400

    leetcode 6/22/25

--- leetcode/Max+min_depth_of_tree/main.cpp
+++ leetcode/Max+min_depth_of_tree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 //Struct provided from leetcode
 struct TreeNode 
 {
@@ -9,8 +10,6 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
-
-
 int maxDepth(TreeNode* root) 
 {
     if(!root)
@@ -19,28 +18,25 @@ int maxDepth(TreeNode* root)
     int depthR = maxDepth(root -> right);
     return std::max(depthL, depthR) + 1;
 }
+
+
+int minDepth(TreeNode* root) //I had to get help for this one again
+{
+    if (!root) return 0; //If root is null, it is 0
+    if (!(root -> left) && !(root -> right)) return 1; //If node is a leaf, it counts as the start of height counting
+    if(!root -> left) return 1 + minDepth(root -> right); //If left node does not exist, we traverse the right one
+    if(!root -> right) return 1 + minDepth(root -> left); //If right node does not exist, we traverse the left one
+    return std::min(1 + minDepth(root -> left), 1 + minDepth(root -> right)); //We're looking for mins, so thus we return the min between the two
+}
+
+
     
 int main()
 {
     TreeNode test = {3, new TreeNode(9, nullptr, nullptr), new TreeNode(20, new TreeNode(15, nullptr,nullptr), new TreeNode(7, nullptr, nullptr))};
     // TreeNode test = {1, nullptr, new TreeNode(2, nullptr, nullptr)};
-    std::cout << maxDepth(&test);
+    std::cout << maxDepth(&test) << '\n';
+    std::cout << minDepth(&test);
 }
 
 
-/* void helper(TreeNode* root, int& depth, bool& goUp)
-{
-    
-
-    if(!root)
-    {
-        goUp = true;
-        return;
-    }
-    
-    if(goUp)
-        ++depth;
-
-    helper(root -> left, depth, goUp);
-    helper(root -> right, depth, goUp);
-} */
