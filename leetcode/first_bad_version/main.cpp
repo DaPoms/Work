444c234b0c149b3090141ae671d87a17f1b5ed47
DaPoms <DanielTripolitan@gmail.com>
2025-07-31 16:59:04 -0400

    Leetcode 7/31/25 (Code cannot work on vsCode as it uses an API)

---
+++ leetcode/first_bad_version/main.cpp
@@ -0,0 +1,25 @@
+//NOTE: This problem does not work on vs code due to the "isBadVersion" API being utilized for the problem, so read it only
+// The API isBadVersion is defined for you as:
+// bool isBadVersion(int version);
+
+/* 
+int helper(long low, long high) //All I did was a binary search but for calling the API isBadVersion
+{
+    long mid = (low + high) / 2;
+    bool target = isBadVersion(mid);
+    bool leftOfTarget = isBadVersion(mid - 1);
+    if(target == true && leftOfTarget == false) //If this is the first bad version, we found it!
+        return mid;
+    else if(target == true) //If this is a bad version, but not the first one, we need to go left to find the first bad version
+        return helper(low, mid - 1);
+    else //If the middle version value is a good version, we need to move right to find the bad version
+        return helper(mid + 1, high);
+}
+int firstBadVersion(int n) 
+{
+    if(n == 1) //edge case
+        return 1;
+    else
+        return helper(1, n);
+}
+ */
