e9d089ffeb8c4d0185be2b8cbb571500201936fe
DaPoms <DanielTripolitan@gmail.com>
2025-11-20 17:56:57 -0500

    Syncing push

--- SQL practice/kimtay.sql
+++ SQL practice/kimtay.sql
@@ -86,12 +86,22 @@ select first_name, last_name from customer left join invoices using (cust_id)
 -- extra credit code
 select first_name, last_name, amount_purchased from customer c JOIN
 (
-    select sum(quantity) as amount_purchased, cust_id from invoice_line il join invoices i using (invoice_num)
+    select 
+        sum(quantity) as amount_purchased, 
+        cust_id 
+        from invoice_line il 
+        join invoices i 
+        using (invoice_num)
         group by cust_id
 ) using (cust_id)
 where amount_purchased IN
 (
-	select max(sum(quantity)) as amount_purchased from invoice_line il join invoices i using (invoice_num) group by cust_id -- groups the customer with the most quantity
+	select 
+        max(sum(quantity)) 
+    from invoice_line il 
+    join invoices i 
+    using (invoice_num) 
+    group by cust_id -- groups the customer with the most quantity
 );
 
 
@@ -111,6 +121,7 @@ SELECT
     amount_purchased 
 FROM customer c 
 JOIN customer_total_purchased ctp
-USING (cust_ID)
-where ctp.amount_purchased = (SELECT MAX(amount_purchased) FROM customer_total_purchased);
+USING (cust_ID) -- effectively adds on the total quantities purchased to each customer
+where amount_purchased = (SELECT MAX(amount_purchased) FROM customer_total_purchased); 
+-- isolates max quantity purchased case
 
