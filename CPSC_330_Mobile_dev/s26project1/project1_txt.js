0736ec4d821b84b89d4565ce11b1d5dd66f4472d
DaPoms <DanielTripolitan@gmail.com>
2026-02-18 10:46:42 -0500

    importing  mobile dev project

---
+++ CPSC_330_Mobile_dev/s26project1/project1_txt.js
@@ -0,0 +1,18 @@
+/*    
+      Project 1
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: 
+      Date:   
+
+      Filename: project1.js
+ */
+ 
+
+
+// You can use chapter 2 code as a reference
+
+// Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
